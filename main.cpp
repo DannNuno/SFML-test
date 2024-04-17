71f2fe9a3ea08303f0723d585e1d01a224436224
Daniela Nuño <13044@portalcedi.edu.mx>
2024-04-17 14:51:37 -0600

    cambios

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include <SFML/Graphics.hpp>
 using namespace sf;
+
+float speedX = 5.f;
+float speedY = 5.f;
+
 int main()
 {
-    RenderWindow window(VideoMode(200, 200), "SFML works!");
-    CircleShape shape(100.f);
-    shape.setFillColor(Color::Green);
+    RenderWindow window(VideoMode(800, 600), "SFML works!");
+    RectangleShape rectangulo({100.f, 50.f});
+    /*vector para el tamaño del rectangulo
+    .f significa float
+    */
+    rectangulo.setFillColor(Color::Magenta);
+    window.setFramerateLimit(60);
 
     while (window.isOpen())
     {
@@ -16,7 +24,28 @@ int main()
         }
 
         window.clear();
-        window.draw(shape);
+
+        if(rectangulo.getPosition().x + rectangulo.getSize().x >= 800 || rectangulo.getPosition().x < 0){
+            speedX *= -1;
+            rectangulo.setFillColor({rand()%255,rand()%255,rand()%255});
+        }
+
+        if(Mouse::isButtonPressed(Mouse::Left)){
+            rectangulo.rotate(15);
+        }
+
+        if(rectangulo.getPosition().y + rectangulo.getSize().y >= 600 || rectangulo.getPosition().y < 0){
+            speedY *= -1;
+            rectangulo.setFillColor({rand()%255,rand()%255,rand()%255});
+        }
+
+        if(Mouse::isButtonPressed(Mouse::Right)){
+            rectangulo.rotate(-15);
+        }
+
+        rectangulo.move({speedX,speedY});
+        //para mover la figura
+        window.draw(rectangulo);
         window.display();
     }
 
